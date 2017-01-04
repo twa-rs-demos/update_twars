abb290f5f47e7614f0ec4bb12f12017a3cced93b
Afaren <550047450@qq.com>
2017-01-04 18:32:38 +0800

    afaren - finish

--- update_twars.sh
+++ update_twars.sh
@@ -1,4 +1,6 @@
-#! /usr/bin/env bash
+#! /usr/bin/env  bash 
+
+set -x
 
 #=========================================================================
 #
@@ -10,38 +12,56 @@
 #
 #=========================================================================
 
+twars='/home/afaren/twars'	# path of Recruiting System, change it in your own computer
+web_api_port=3000
+
 
 update_web () {
 	echo "update web"
+	update_static_file "$twars/web"
 }
 
 update_teacher_admin_web () {
 	echo "update teacher admin web"
+	update_static_file "$twars/teacher-admin-web"
 }
 
 update_web_api () {
 	echo "update web api"
+	cd "$twars/web-api" || exit 1
+	git pull -r
+	npm install 
+	npm run refreshMongo
+	kill $(lsof -t -i:$web_api_port)
+	nohup npm start > /dev/null &
 }
 
 update_paper_api () {
 	echo "update paper api"
+	cd "$twars/paper-api" || exit 1
+	git pull -r
+	./gradlew clean flywayclean flywaymigrate
+	./gradlew jettyRestart || nohup ./gradlew jettyStart > /dev/null &
 }
-
 
 update_static_file () {
-	update_web
-	update_teacher_admin_web
+	cd "$1" || exit 1
+	git pull -r
+	npm install 
+	npm run webpack
 }
-
 
 update_web_service () {
 	update_web_api
 	update_paper_api
 }
 
+update_twars () {
+	update_web 
+	update_teacher_admin_web
+	update_paper_api
+	update_web_api
+}
 
+update_twars
 
-
-
-
-update_static_file && update_static_file 
